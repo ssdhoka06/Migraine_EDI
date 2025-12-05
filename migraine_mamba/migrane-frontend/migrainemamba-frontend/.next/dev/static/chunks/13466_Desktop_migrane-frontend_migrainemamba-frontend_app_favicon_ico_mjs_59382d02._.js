f61134136e9daaebb6bc0ff7f68c240770bbe966
ssdhoka06 <sachidhoka@gmail.com>
2025-12-05 10:48:28 +0530

    Delete migraine_mamba/migrane-frontend/migrainemamba-frontend directory

--- migraine_mamba/migrane-frontend/migrainemamba-frontend/.next/dev/static/chunks/13466_Desktop_migrane-frontend_migrainemamba-frontend_app_favicon_ico_mjs_59382d02._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/65b25_next_dist_416738ee._.js"
-],
-    source: "dynamic"
-});
